b4c71fa6cbc26b181f8777de2a19c512900630bd
Anvith Bhat <anvithv4@gmail.com>
2017-01-17 11:09:45 +0530

    Formatting and other stuff

--- CenterLockListener.java
+++ CenterLockListener.java
@@ -1,107 +1,110 @@
 package com.demo;
 
-import android.content.Context;
-import android.graphics.Point;
+
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Display;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 
 public class CenterLockListener extends RecyclerView.OnScrollListener {
 
-    //To avoid recursive calls
-    private boolean mAutoSet = true;
+    //Flag to avoid recursive calls
+    private boolean isCascadedCall = true;
 
     //The pivot to be snapped to
-    private int mCenterPivot;
+    private int centerPivot;
 
 
-    public CenterLockListener(int center){
-
-        this.mCenterPivot = center;
-
+    public CenterLockListener(int centerPivot) {
+        this.centerPivot = centerPivot;
     }
 
+
+    /**
+     * Overrides the scroll state changes and
+     * repositions the scroll to lock the view
+     * to the center
+     *
+     * @param recyclerView
+     * @param newState
+     */
     @Override
     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
         super.onScrollStateChanged(recyclerView, newState);
 
         LinearLayoutManager lm = (LinearLayoutManager) recyclerView.getLayoutManager();
 
-        if( mCenterPivot == 0 ) {
+        // Default pivot , Its a bit inaccurate .
+        // Better pass the center pivot as your Center Indicator view's
+        // calculated center on it OnGlobalLayoutListener event
 
-            // Default pivot , Its a bit inaccurate .
-            // Better pass the center pivot as your Center Indicator view's
-            // calculated center on it OnGlobalLayoutListener event
-            mCenterPivot = lm.getOrientation() == LinearLayoutManager.HORIZONTAL ? ( recyclerView.getLeft() + recyclerView.getRight() ) : ( recyclerView.getTop() + recyclerView.getBottom() );
+        if (centerPivot == 0) {
+            centerPivot = lm.getOrientation() == LinearLayoutManager.HORIZONTAL ? (recyclerView.getLeft() + recyclerView.getRight()) : (recyclerView.getTop() + recyclerView.getBottom());
         }
-        if( !mAutoSet ) {
 
-            if( newState == RecyclerView.SCROLL_STATE_IDLE ) {
-                //ScrollStoppped
+        if (!isCascadedCall) {
+
+            //Scroll has stopped we can now proceed with computations
+            if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+
                 View view = findCenterView(lm);//get the view nearest to center
-                int viewCenter = lm.getOrientation() == LinearLayoutManager.HORIZONTAL ? ( view.getLeft() + view.getRight() )/2 :( view.getTop() + view.getBottom() )/2;
-                //compute scroll from center
-                int scrollNeeded = viewCenter - mCenterPivot; // Add or subtract any offsets you need here
+                int viewCenter = lm.getOrientation() == LinearLayoutManager.HORIZONTAL ? (view.getLeft() + view.getRight()) / 2 : (view.getTop() + view.getBottom()) / 2;
 
-                if( lm.getOrientation() == LinearLayoutManager.HORIZONTAL ) {
+                // Compute scroll from center
+                // Add or subtract any offsets you need here
+                int scrollNeeded = viewCenter - centerPivot;
 
+                if (lm.getOrientation() == LinearLayoutManager.HORIZONTAL) {
                     recyclerView.smoothScrollBy(scrollNeeded, 0);
+                } else {
+                    recyclerView.smoothScrollBy(0, scrollNeeded);
                 }
-                else
-                {
-                    recyclerView.smoothScrollBy(0, (int) (scrollNeeded));
 
-                }
-                mAutoSet =true;
+                isCascadedCall = true;
             }
         }
-        if( newState == RecyclerView.SCROLL_STATE_DRAGGING || newState == RecyclerView.SCROLL_STATE_SETTLING ){
 
-            mAutoSet =false;
+        if (newState == RecyclerView.SCROLL_STATE_DRAGGING || newState == RecyclerView.SCROLL_STATE_SETTLING) {
+            isCascadedCall = false;
         }
     }
 
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
-
     }
-    private View findCenterView(LinearLayoutManager lm) {
 
-        int minDistance = 0;
+    /**
+     * This method iterates through the visible views and
+     * figures out the
+     *
+     * @param lm - The LinearLayoutManager of the RecyclerView
+     * @return View nearest to the center
+     */
+    private View findCenterView(LinearLayoutManager lm) {
         View view = null;
         View returnView = null;
-        boolean notFound = true;
 
-        for(int i = lm.findFirstVisibleItemPosition(); i <= lm.findLastVisibleItemPosition() && notFound ; i++ ) {
+        int minDistance = 0;
+        boolean isNotFound = true;
 
-            view=lm.findViewByPosition(i);
+        for (int i = lm.findFirstVisibleItemPosition(); i <= lm.findLastVisibleItemPosition() && isNotFound; i++) {
 
-            int center = lm.getOrientation() == LinearLayoutManager.HORIZONTAL ? ( view.getLeft() + view.getRight() )/ 2 : ( view.getTop() + view.getBottom() )/ 2;
-            int leastDifference = Math.abs(mCenterPivot - center);
+            view = lm.findViewByPosition(i);
 
-            if( leastDifference <= minDistance || i == lm.findFirstVisibleItemPosition())
-            {
+            int center = lm.getOrientation() == LinearLayoutManager.HORIZONTAL ? (view.getLeft() + view.getRight()) / 2 : (view.getTop() + view.getBottom()) / 2;
+            int leastDifference = Math.abs(centerPivot - center);
+
+            if (leastDifference <= minDistance || i == lm.findFirstVisibleItemPosition()) {
                 minDistance = leastDifference;
-                returnView=view;
-            }
-            else
-            {
-                notFound=false;
+                returnView = view;
+            } else {
+                isNotFound = false;
 
             }
         }
         return returnView;
     }
-
-
 
 
 }
